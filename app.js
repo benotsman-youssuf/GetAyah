300ae1d888c619971a8125a9bce9c857c4bcd6c7
youssef <benotsman.youssuf@gmail.com>
2024-04-11 04:35:26 +0100

    organise code js

--- app.js
+++ app.js
@@ -1,66 +1,65 @@
-const surrahElement = document.getElementById('surrah');
-const ayaNumElement = document.getElementById('aya-num');
-const ayaElement = document.getElementById('aya');
-const btnElement = document.getElementById('butn');
-const audioElement = document.getElementById('audio');
-const tafseerElement = document.getElementById('tafsir');
+const surrahElement = document.getElementById("surrah");
+const ayaNumElement = document.getElementById("aya-num");
+const ayaElement = document.getElementById("aya");
+const btnElement = document.getElementById("butn");
+const audioElement = document.getElementById("audio");
+const tafseerElement = document.getElementById("tafsir");
+const moyassarElement = document.getElementById("moyassar");
+const saadiElement = document.getElementById("saadi");
+const enElement = document.getElementById("en");
 
 let surrahNum, ayahNum;
 
 const fetchAya = async () => {
-    try {
-        let random = getRandomInt(1, 6236);
-        const response = await fetch(`https://api.alquran.cloud/v1/ayah/${random}/ar.alafasy`);
-        const data = await response.json();
+  try {
+    const random = getRandomInt(1, 6236);
+    const response = await fetch(
+      `https://api.alquran.cloud/v1/ayah/${random}/ar.alafasy`
+    );
+    const data = await response.json();
 
-        const surrah = data.data.surah.name;
-        const ayah = data.data.text;
-        surrahNum = data.data.surah.number;
-        ayahNum = data.data.numberInSurah;
+    const surrah = data.data.surah.name;
+    const ayah = data.data.text;
+    surrahNum = data.data.surah.number;
+    ayahNum = data.data.numberInSurah;
 
-        surrahElement.textContent = surrah;
-        ayaElement.textContent = ayah;
-        ayaNumElement.textContent = ayahNum;
-        audioElement.src = `https://cdn.islamic.network/quran/audio/128/ar.alafasy/${random}.mp3`;
-        fetchTafseer(1);
-    } catch (error) {
-        console.log('error');
-    }
-}
-
-
-const moyassarElement = document.getElementById('moyassar');
-const saadiElement = document.getElementById('saadi');
-const enElement = document.getElementById('en');
+    surrahElement.textContent = surrah;
+    ayaElement.textContent = ayah;
+    ayaNumElement.textContent = ayahNum;
+    audioElement.src = `https://cdn.islamic.network/quran/audio/128/ar.alafasy/${random}.mp3`;
+    fetchTafseer(1);
+  } catch (error) {
+    console.error("Error fetching Aya:", error);
+  }
+};
 
 const fetchTafseer = async (tafsiirId) => {
-    try {
-        const response = await fetch(`http://api.quran-tafseer.com/tafseer/${tafsiirId}/${surrahNum}/${ayahNum}`);
-        const data = await response.json();
-        const tafseer = data.text;
-        tafseerElement.textContent = tafseer;
-    } catch (error) {
-        console.log('error');
-    }
-}
+  try {
+    const response = await fetch(
+      `http://api.quran-tafseer.com/tafseer/${tafsiirId}/${surrahNum}/${ayahNum}`
+    );
+    const data = await response.json();
+    const tafseer = data.text;
+    tafseerElement.textContent = tafseer;
+  } catch (error) {
+    console.error("Error fetching Tafseer:", error);
+  }
+};
 
-btnElement.addEventListener('click', fetchAya);
-moyassarElement.addEventListener('click', () => fetchTafseer(1));
-saadiElement.addEventListener('click', () => fetchTafseer(3));
-enElement.addEventListener('click', () => fetchTafseer(10));
-
-
+btnElement.addEventListener("click", fetchAya);
+moyassarElement.addEventListener("click", () => fetchTafseer(1));
+saadiElement.addEventListener("click", () => fetchTafseer(3));
+enElement.addEventListener("click", () => fetchTafseer(10));
 
 function getRandomInt(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+const buttons = document.querySelectorAll("#tafasir > button");
 
-const buttons = document.querySelectorAll('#tafasir > button');
-
-buttons.forEach(button => {
-    button.addEventListener('click', () => {
-    buttons.forEach(btn => btn.classList.remove('active'));
-    button.classList.add('active');
-    });
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    buttons.forEach((btn) => btn.classList.remove("active"));
+    button.classList.add("active");
+  });
 });
